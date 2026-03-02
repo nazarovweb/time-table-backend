1b72a2d65c4debf68b94fbb33e893cc97af5eb14
Elshod Nazarov <e.nazarov@student.pdp.university>
2026-03-02 10:10:19 +0500

    check cors

--- src/app.js
+++ src/app.js
@@ -7,26 +7,25 @@ async function createApp() {
     const app = express();
     app.use(express.json());
 
-const allowlist = [
-  "http://localhost:3030",
-  "https://time-table-frontend-06.vercel.app",
-  "https://time-table-backend-4o64.onrender.com",
-];
+    const allowlist = [
+        "http://localhost:3030",
+        "https://time-table-frontend-06.vercel.app",
+        "https://time-table-backend-4o64.onrender.com",
+    ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // Swagger yoki Postman origin yubormasligi mumkin (undefined)
-    if (!origin) return callback(null, true);
+    app.use(cors({
+        origin: function (origin, callback) {
+            // Swagger yoki Postman origin yubormasligi mumkin (undefined)
+            if (!origin) return callback(null, true);
 
-    if (allowlist.includes(origin)) return callback(null, true);
+            if (allowlist.includes(origin)) return callback(null, true);
 
-    return callback(new Error("Not allowed by CORS: " + origin));
-  },
-  credentials: true, // agar cookie/session ishlatsang kerak bo‘ladi
-}));
+            return callback(new Error("Not allowed by CORS: " + origin));
+        },
+        credentials: true, // agar cookie/session ishlatsang kerak bo‘ladi
+    }));
 
-// Preflight (OPTIONS) requestlar uchun:
-app.options("*", cors());    // AdminJS (async because of ESM dynamic imports)
+    // AdminJS (async because of ESM dynamic imports)
     const { admin, router } = await buildAdminRouter();
     app.use(admin.options.rootPath, router);
 
